b9e0319d604326c8d759b37b0cbb43cb71746fa2
shehap <115353206+shehab400@users.noreply.github.com>
2024-06-06 16:14:39 +0300

    bugfixes + new arrangment

--- pythonfiles/main.py
+++ pythonfiles/main.py
@@ -1,7 +1,8 @@
 from myWindow import *
-
+import qdarkstyle
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)  
     window = MyWindow()
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) 
